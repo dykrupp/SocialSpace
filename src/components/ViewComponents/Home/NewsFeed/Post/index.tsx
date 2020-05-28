e9f7ccbe6fa2e2eb32987ff0ae2d0b72c2e54fb9
Dylan Krupp <dylan.krupp@promessinc.com>
2020-05-28 16:25:33 -0400

    created newsFeed component, added media to post state

---
+++ src/components/ViewComponents/Home/NewsFeed/Post/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Paper, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+//TODO -> Continue work here -> Make UI for media and status -> timestamp & user required
+
+interface PostProps {
+  post: string;
+  username: string;
+  dateTime: string;
+}
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '20px',
+  },
+  gridRow: {
+    display: 'flex',
+  },
+}));
+
+const Post: React.FC<PostProps> = ({ post, username, dateTime }) => {
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={3} className={classes.paper}>
+      <Grid container spacing={1}>
+        <Grid item xs={12} className={classes.gridRow}>
+          <p>{username}</p>
+          <p style={{ marginLeft: '20px' }}>
+            {new Date(Date.parse(dateTime)).toLocaleString()}
+          </p>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">{post}</Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+Post.propTypes = {
+  post: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  dateTime: PropTypes.string.isRequired,
+};
+
+export default Post;
---
+++ src/components/ViewComponents/Home/NewsFeed/index.tsx
@@ -0,0 +1,98 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-undef */
+import React, { useEffect, useContext, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CreatePost from './CreatePost';
+import { FirebaseContext } from '../../../Firebase/context';
+import { AuthUserContext } from '../../../AuthProvider/context';
+import { Grid } from '@material-ui/core';
+import Post from './Post';
+import * as Collections from 'typescript-collections';
+
+interface Post {
+  post: string;
+  dateTime: string;
+  media: File | null;
+}
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '500px',
+    margin: '0 auto',
+  },
+}));
+
+const NewsFeed: React.FC = () => {
+  const firebase = useContext(FirebaseContext);
+  const authUser = useContext(AuthUserContext);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const classes = useStyles();
+
+  const getSortedPosts = (snapShot: firebase.database.DataSnapshot): Post[] => {
+    const postsObject = snapShot.val();
+    const currentPosts: Post[] = Object.keys(postsObject).map((key) => ({
+      ...postsObject[key],
+      dateTime: key,
+    }));
+    return currentPosts.sort((a, b) => {
+      const secondDate: any = new Date(b.dateTime);
+      const firstDate: any = new Date(a.dateTime);
+      return secondDate - firstDate;
+    });
+  };
+
+  //Get all posts w/ associated media (eventually just filter to users that are being 'followed')
+  useEffect(() => {
+    if (firebase && authUser) {
+      firebase.posts(authUser.uid).on('value', (snapShot) => {
+        const sortedPosts = getSortedPosts(snapShot);
+        const storageRef = firebase.storage.ref(`users/${authUser.uid}/posts/`);
+
+        //grab all associated media relative to the uid before setting posts state
+        storageRef.listAll().then((list) => {
+          new Promise<Collections.Dictionary<string, any>>((resolve) => {
+            const dict = new Collections.Dictionary<string, any>();
+            list.prefixes.forEach((prefix, index, array) => {
+              prefix
+                .child('media')
+                .getDownloadURL()
+                .then((url) => {
+                  dict.setValue(prefix.name, url);
+                  if (index === array.length - 1) resolve(dict);
+                });
+            });
+          }).then((dict) => {
+            const postsWithMedia = sortedPosts.map((post) => {
+              if (dict.keys().includes(post.dateTime)) {
+                return { ...post, media: dict.getValue(post.dateTime) };
+              } else return post;
+            });
+            setPosts(postsWithMedia);
+          });
+        });
+      });
+    }
+  }, [firebase, authUser]);
+
+  console.log(posts);
+
+  if (!authUser) return null;
+  return (
+    <Grid container direction="column" spacing={2} className={classes.root}>
+      <Grid item>
+        <CreatePost />
+      </Grid>
+      {posts.map((post: Post) => (
+        <Grid item key={post.dateTime}>
+          <Post
+            post={post.post}
+            username={authUser.fullName}
+            dateTime={post.dateTime}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default NewsFeed;
