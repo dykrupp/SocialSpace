48025e418be8399ce268e9f7bf857c40036f2c68
Dylan Krupp <dylan.krupp@promessinc.com>
2020-06-30 12:20:38 -0400

    Added updateMessageCount() and recountMessages() Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,62 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+//allows client to easily determine new unread messages for the user w/o getting ALL messages back
+export const updateMessageCount = functions.database
+  .ref('/chats/{pushID}/messages')
+  .onWrite(async (change, context) => {
+    const collectionRef = change.after.ref.parent;
+    if (!collectionRef || !collectionRef.parent) return null;
 
-//TODO -> Just added as a proof in concept for Cloud Functions -> used to call on the client side
-// const onMessageClick = (): void => {
-//   if (firebase) {
-//     const helloWorld = firebase.functions.httpsCallable('helloWorld');
-//     helloWorld().then((result) => console.log(result));
-//   }
-// };
+    let modifier = 0;
+    if (
+      (change.after.exists() && !change.before.exists()) ||
+      (change.after.exists() && change.before.exists())
+    ) {
+      modifier = 1;
+    } else if (!change.after.exists() && change.before.exists()) {
+      modifier = -1;
+    } else {
+      return null;
+    }
 
-export const helloWorld = functions.https.onCall((data, context) => {
-  return `Hello ${context.auth?.token.name}, Firebase Cloud Functions says hi!`;
-});
+    const chatUID = collectionRef.key;
+    if (!chatUID) return null;
+
+    const chatUIDRef = collectionRef.parent.parent
+      ?.child('chatUIDS')
+      .child(chatUID);
+    if (!chatUIDRef) return null;
+
+    await chatUIDRef.child('message_count').transaction((current) => {
+      return change.after.val() !== null ? (current || 0) + modifier : 0;
+    });
+
+    await chatUIDRef
+      .child('last_write_uid')
+      .transaction(() => context.auth?.uid);
+
+    return null;
+  });
+
+//Fixes the database if the message_count is deleted
+export const recountMessages = functions.database
+  .ref('/chatUIDS/{pushID}/message_count')
+  .onDelete(async (snapShot) => {
+    const counterRef = snapShot.ref;
+    const chatUID = snapShot.ref.parent?.key;
+
+    //don't recreate node if the messages are empty
+    if (snapShot.val() === 0) return;
+
+    if (!counterRef || !chatUID) return null;
+
+    const collectionRef = counterRef.parent?.parent?.parent
+      ?.child('chats')
+      .child(chatUID)
+      .child('messages');
+    if (!collectionRef) return null;
+
+    const messagesData = await collectionRef.once('value');
+    await counterRef.set(messagesData.numChildren());
+    return null;
+  });
